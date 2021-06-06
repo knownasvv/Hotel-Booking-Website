863d614a25b60a4c6a1fd51d722c83a9a3c4078a
kakaranara <wahyu.koco@student.umn.ac.id>
2021-06-06 15:00:26 +0700

    login adjustment

---
+++ application/views/include/loginStyle.php
@@ -0,0 +1,6 @@
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="<?= base_url('assets/loginAssets/fonts/material-icon/css/material-design-iconic-font.min.css')?>">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="<?= base_url('assets/loginAssets/css/style.css')?>">
