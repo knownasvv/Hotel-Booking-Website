18b33cbe233a390e4e549a03c8cd27ace68a0e6e
kakaranara <wahyu.koco@student.umn.ac.id>
2021-06-06 14:48:44 +0700

    model login

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,14 +1,14 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed !');
 
 class User_model extends CI_Model {
-    //PERHATIAN !! PAKAI CAMEL CASE SAJA DISINI YA
+    //PERHATIAN !! GUNAKAN CAMEL CASE DEMI MENJAGA KERAPIHAN
     //ex : getModel()
     public function __construct() {
         parent::__construct();
         $this->load->database();
     }
     
-    function get_user($email = null, $password = null){
+    function getUser($email = null, $password = null){
 		if($email == null && $password == null) $query = $this->db->get('users');
 		else $query = $this->db->get_where('users', array('email' => $email, 'password' => $password));
         return $query->result_array();
