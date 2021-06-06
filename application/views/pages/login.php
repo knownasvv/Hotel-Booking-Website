eefb7b19e364c0b5f4f10f076dd024947a74be95
kakaranara <wahyu.koco@student.umn.ac.id>
2021-06-06 19:39:03 +0700

    valid login captcha

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,38 +9,45 @@
 
     <?= $loginStyle ?>
 </head>
+
 <body>
-
     <div class="main">
-
-        
         <!-- Sing in  Form -->
         <section class="sign-in">
             <div class="container">
                 <div class="signin-content">
                     <div class="signin-image">
-                        <figure><img src="<?= base_url('assets/loginAssets/images/signin-image.jpg')?>" alt="sing up image"></figure>
-                        <a href="#" class="signup-image-link">Create an account</a>
+                        <figure><img src="<?= base_url('assets/loginAssets/images/signin-image.jpg') ?>" alt="sing up image"></figure>
+                        <a href="<?= base_url('index.php/Register') ?>" class="signup-image-link">Create an account</a>
                     </div>
 
                     <div class="signin-form">
                         <h2 class="form-title">Login</h2>
-                        <form method="POST" class="register-form" id="login-form">
+                        <form action="<?= base_url('index.php/Login/validate') ?>" method="POST" class="register-form" id="login-form">
                             <div class="form-group">
                                 <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="email" placeholder="Your Name" required/>
+                                <input type="email" name="email" placeholder="Your Name" required />
                             </div>
                             <div class="form-group">
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="password"placeholder="Password" required/>
+                                <input type="password" name="password" placeholder="Password" required />
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
                                 <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
                             </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                            <div class="form-group">
+                                 <?= $image ?> <!-- Captcha img -->
                             </div>
+                            <div class="form-group">
+                                <input type="text" name="captcha" required class="form Control" placeholder="Enter Captcha Here">
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
+                            </div>
+
+                            <?php  ?>
+
                         </form>
                         <div class="social-login">
                             <span class="social-label">Or login with</span>
@@ -58,6 +66,7 @@
 
     <?= $loginScript ?>
 </body>
+
 </html>
 
 <!-- This templates was made by Colorlib (https://colorlib.com) -->
