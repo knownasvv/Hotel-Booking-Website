de1f536aa78cb0ccae60130d8f7b03c297c5d04d
kakaranara <wahyu.koco@student.umn.ac.id>
2021-06-09 16:10:32 +0700

    aboutus info (u can edit)

--- application/views/pages/aboutus.php
+++ application/views/pages/aboutus.php
@@ -1,46 +1,67 @@
 <?= $header ?>
 <section class="py-5" id="about" style="background-color: #fff;">
-		<div class="container-fluid">
-			<div class="container">
-				<h5 style="text-align:center"><strong> ABOUT US </strong></h5><hr>
-				<div class="row justify-content-center text-center px-3 pb-4">
-					<div class="col-12 mb-3">
-						<h6 class="card-title mt-1">This website is made by</h6>
-						<div class="row row-cols-4 row-cols-md-2 g-4">
-							<div class="col">
-								<div class="card">
-									<img src="<?php echo base_url("assets/images/profile/koco.jpg") ?>" class="card-img-top" alt="...">
-									<div class="card-body bg-light">
+	<div class="container-fluid">
+		<div class="container">
+			<h5 style="text-align:center"><strong> ABOUT US </strong></h5>
+			<hr>
+			<div class="row justify-content-center text-center px-3 pb-4">
+				<div class="col-12 mb-3">
+					<h6 class="card-title mt-1">This website is made by</h6>
+					<div class="row row-cols-4 row-cols-md-2 g-4">
+						<div class="col">
+							<div class="card">
+								<img src="<?php echo base_url("assets/images/profile/koco.jpg") ?>" class="card-img-top" alt="...">
+								<div class="card-body bg-light">
 									<h5 class="card-title">Wahyu Koco</h5>
 									<p class="card-text">00000040112</p>
-									</div>
+									<p class="mb-0">Peran : </p>
+									<ul>
+										<li class="text-left">Database Maker</li>				
+										<li class="text-left">Login & Register & profile user</li>
+										<li class="text-left">About Us</li>
+									</ul>
 								</div>
 							</div>
-							<div class="col">
-								<div class="card">
-									<img src="<?php echo base_url("assets/images/profile/veren.png") ?>" class="card-img-top" alt="...">
-									<div class="card-body bg-light">
+						</div>
+						<div class="col">
+							<div class="card">
+								<img src="<?php echo base_url("assets/images/profile/veren.png") ?>" class="card-img-top" alt="...">
+								<div class="card-body bg-light">
 									<h5 class="card-title">Veren Valensia</h5>
 									<p class="card-text">00000040923</p>
-									</div>
+									<p class="mb-0">Peran : </p>
+									<ul>
+										<li class="text-left">Main Front-End</li>				
+										<li class="text-left">Invoices</li>
+										<li class="text-left">Database Maker</li>
+									</ul>
 								</div>
 							</div>
-							<div class="col">
-								<div class="card">
-									<img src="<?php echo base_url("assets/images/profile/vincent.jpg") ?>" class="card-img-top" alt="...">
-									<div class="card-body bg-light">
+						</div>
+						<div class="col">
+							<div class="card">
+								<img src="<?php echo base_url("assets/images/profile/vincent.jpg") ?>" class="card-img-top" alt="...">
+								<div class="card-body bg-light">
 									<h5 class="card-title">Vincent Christopher</h5>
 									<p class="card-text">00000040754</p>
-									</div>
+									<p class="mb-0">Peran : </p>
+									<ul>
+										<li class="text-left">CRUD Model</li>				
+										<li class="text-left">Search filter</li>
+									</ul>
 								</div>
 							</div>
-							<div class="col">
-								<div class="card">
-									<img src="<?php echo base_url("assets/images/profile/feiza.png") ?>" class="card-img-top" alt="...">
-									<div class="card-body bg-light">
+						</div>
+						<div class="col">
+							<div class="card">
+								<img src="<?php echo base_url("assets/images/profile/feiza.png") ?>" class="card-img-top" alt="...">
+								<div class="card-body bg-light">
 									<h5 class="card-title">Feiza Joane</h5>
 									<p class="card-text">00000040118</p>
-									</div>
+									<p class="mb-0">Peran : </p>
+									<ul>
+										<li class="text-left">Mock-up Designer</li>				
+									</ul>
 								</div>
 							</div>
 						</div>
@@ -48,5 +69,6 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
 <?= $footer ?>
