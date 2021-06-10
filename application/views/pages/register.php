1d61698bd4d393c866929a70be2aae1a4c79728a
kakaranara <wahyu.koco@student.umn.ac.id>
2021-06-10 17:45:34 +0700

    add features for register (input wont dissapear )

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -20,13 +20,13 @@
                         <?php echo form_open('Register/validate') ?>
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="nama" id="name" placeholder="Your Name"/>
+                                <input type="text" name="nama" id="name" placeholder="Your Name" value="<?php if(isset($_POST['nama'])) echo $_POST['nama'];    ?>"/>
                             </div>
                             <?= form_error('nama', "<div style='color: red;'>", "</div>") ?>
 
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                                <input type="email" name="email" id="email" placeholder="Your Email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>"/>
                             </div>
                             <?= form_error('email', "<div style='color: red;'>", "</div>") ?>
                             <div class="form-group">
@@ -41,12 +41,12 @@
                             <?= form_error('confirm_password', "<div style='color: red;'>", "</div>") ?>
                             <div class="form-group">
                                 <label for="notelp"><i class="zmdi zmdi-phone"></i></label>
-                                <input type="number" name="notelp" id="notelp" placeholder="Nomor Telepon"/>
+                                <input type="number" name="notelp" id="notelp" placeholder="Nomor Telepon" value="<?php if(isset($_POST['notelp'])) echo $_POST['notelp']; ?>"/>
                             </div>
                             <?= form_error('notelp', "<div style='color: red;'>", "</div>") ?>
                             <div class="form-group">
                                 <label for="tgl"><i class="zmdi zmdi-calendar-note"></i></label>
-                                <input type="date" name="tanggal_lahir" id="tgl"/>
+                                <input type="date" name="tanggal_lahir" id="tgl" value="<?php if(isset($_POST['tanggal_lahir'])) echo $_POST['tanggal_lahir']; ?>"/>
                             </div>
                             <?= form_error('tanggal_lahir', "<div style='color: red;'>", "</div>") ?>                      
                             <div class="form-group">
