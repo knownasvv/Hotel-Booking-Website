730a7fb309ae60b4a66ac8ad34c1bf34eb820332
kakaranara <wahyu.koco@student.umn.ac.id>
2021-06-10 20:16:44 +0700

    style register

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -51,12 +51,12 @@
                             <input type="date" name="tanggal_lahir" id="tgl" value="<?php if (isset($_POST['tanggal_lahir'])) echo $_POST['tanggal_lahir']; ?>" />
                         </div>
                         <?= form_error('tanggal_lahir', "<div style='color: red;'>", "</div>") ?>
-                        <div class="form-group">
+                        <div class="form-group"> Upload Foto : 
                             <input type="file" name="foto">
                         </div>
                         <div class="form-group">
                             <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in <a href="#" class="term-service">Terms of service</a></label>
+                            <label for="agree-term" required class="label-agree-term"><span><span></span></span>I agree all statements in <a href="#" class="term-service">Terms of service</a></label>
                         </div>
                         <div class="form-group form-button">
                             <input type="submit" name="submit" id="signup" class="form-submit" value="Register" />
